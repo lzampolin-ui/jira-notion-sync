887f15a7c8e113340ccf3995574dfd9a802218da
lzampolin-ui <lzampolin@citizenplane.com>
2025-10-07 17:03:00 +0200

    Update sync_jira_notion.py

--- sync_jira_notion.py
+++ sync_jira_notion.py
@@ -1,5 +1,5 @@
 JQL_QUERY = project = "GMI"
-AND "client[dropdown]" = "US BANGLA"
+AND "client" = "US BANGLA"
 AND status NOT IN (Completed, REJECTED, ROLLBACKED)
-AND "show to customer[checkboxes]" = Yes
+AND "show to customer" = Yes
 ORDER BY "cf[10644]" ASC, created DESC
