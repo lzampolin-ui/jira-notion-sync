e42e66acd17a1e1620e818f27ab2dc3bc0028feb
lzampolin-ui <lzampolin@citizenplane.com>
2025-10-08 11:11:11 +0200

    Update sync_jira_notion.py

--- sync_jira_notion.py
+++ sync_jira_notion.py
@@ -1,5 +1,5 @@
-JQL_QUERY = project = "GMI"
+JQL_QUERY = '''project = "GMI"
 AND "client" = "US BANGLA"
 AND status NOT IN (Completed, REJECTED, ROLLBACKED)
 AND "show to customer" = Yes
-ORDER BY "cf[10644]" ASC, created DESC
+ORDER BY "cf[10644]" ASC, created DESC'''
